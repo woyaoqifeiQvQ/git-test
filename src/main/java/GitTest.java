ad7527938279ec676e96793929df1a23e705aba9
permissive <1033932518@qq.com>
2022-07-06 16:47:37 +0800

    the first submission

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * @author Tian Hongfei
+ * @description
+ * @date 2022-07-06    16:44
+ */
+public class GitTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("和奥多久啊口水都快安徽省");
+    }
+}
